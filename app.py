0afb70790e7e58ff93b1ba4918614dc908bb0a3c
Rishu gurjar <gurjarrishu547@gmail.com>
2026-03-28 23:51:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from parser import parse_resume
 
 st.title("AI Resume Parser 🔥")
 
-uploaded_file = st.file_uploader("Upload Resume")
+uploaded_file = st.file_uploader("Upload Resume", type=["pdf"])
 
 if uploaded_file:
-    data = parse_resume(uploaded_file)
-    st.json(data)
+    st.write("Processing...")
+
+    try:
+        data = parse_resume(uploaded_file)
+        st.json(data)
+    except Exception as e:
+        st.error(f"Error: {e}")
